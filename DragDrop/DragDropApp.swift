31126e440fe52ccc318fa6843745ed13aa7130b4
Harshit Parikh <harshit.parikh@bambooapp.com>
2021-04-25 10:17:51 +0530

    Initial Commit

---
+++ DragDrop/DragDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragDropApp.swift
+//  DragDrop
+//
+//  Created by Harshit Parikh on 25/04/21.
+//
+
+import SwiftUI
+
+@main
+struct DragDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
